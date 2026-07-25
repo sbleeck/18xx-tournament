b4a0cbebc91f67f8d78033a1c267959885b91231
Stefan Bleeck <bleeck@gmail.com>
2026-07-25 21:08:46 +0100

    fixed error in L14 brown

--- lib/engine/game/g_1835/map.rb
+++ lib/engine/game/g_1835/map.rb
@@ -239,7 +239,8 @@ module Engine
             'city=revenue:40,slots:2;path=a:0,b:_0;path=a:1,b:_0;path=a:3,b:_0;path=a:4,b:_0;upgrade=cost:50;label=Y',
             ['L14'] =>
             'city=revenue:30,loc:2.5;path=a:3,b:_0;path=a:2,b:_0;'\
-            'city=revenue:30,loc:5.5;path=a:5,b:_1;path=a:0,b:_1;label=XX',
+            'city=revenue:30,loc:5.5;path=a:5,b:_1;path=a:0,b:_1;label=XX;'\
+            'future_label=label:X,color:brown',
           },
           brown: {
             %w[A9 G1] => 'path=a:4,b:5',
